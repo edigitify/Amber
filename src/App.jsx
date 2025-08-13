07e1a82283345c51e581cf40a4533d0a57587d72
Edigitify <abhijeetedigitify@gmail.com>
2025-08-13 23:32:13 +0530

    error solve

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
     <Router>
       <div className="min-h-screen bg-gray-50">
         <Navbar />
-        <main className="pt-16">
+        <main>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
